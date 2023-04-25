cd43bf8e6078c7b7ab791fceafa61c96855fc0d0
isa1wi <almin.isamovic@at.bosch.com>
2023-04-25 15:44:40 +0200

    Smol updates

--- index.js
+++ index.js
@@ -8,13 +8,16 @@ app.use(express.json());
 const port = process.env.PORT || 5000;
 
 const configuration = new Configuration({
-  apiKey: process.env.OPENAI_API_KEY,
+  apiKey: process.env.OPEN_API_KEY,
 });
+console.log("Key initialized", process.env.OPEN_API_KEY);
+console.log(configuration);
 const openai = new OpenAIApi(configuration);
 
 // POST request endpoint
 app.post("/ask", async (req, res) => {
   const prompt = req.body.prompt;
+  console.log("PROMPT:", prompt);
   try {
     if (prompt == null) {
       throw new Error("Uh oh, no prompt was provided");
@@ -35,5 +38,5 @@ app.post("/ask", async (req, res) => {
   }
 });
 
-console.log("Key initialized", process.env.OPENAI_API_KEY);
+console.log("Key initialized", process.env.OPEN_API_KEY);
 app.listen(port, () => console.log(`Server is running on port ${port}!!`));
